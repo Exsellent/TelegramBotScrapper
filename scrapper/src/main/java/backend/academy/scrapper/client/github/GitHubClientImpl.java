9ed9130407c952410a92580788dd2db6408f2174
Excellent <7074172@gmail.com>
2025-02-24 18:12:06 +0300

    Creating StackOverflow and GitHub scrapper application clients

---
+++ scrapper/src/main/java/backend/academy/scrapper/client/github/GitHubClientImpl.java
@@ -0,0 +1,56 @@
+package backend.academy.scrapper.client.github;
+
+import backend.academy.scrapper.dto.IssuesCommentsResponse;
+import backend.academy.scrapper.dto.PullCommentsResponse;
+import backend.academy.scrapper.dto.PullRequestResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+@Service
+public class GitHubClientImpl implements GitHubClient {
+
+    private final WebClient webClient;
+    private final Retry retrySpec;
+
+    public GitHubClientImpl(@Qualifier("gitHubWebClient") WebClient webClient, Retry retrySpec) {
+        this.webClient = webClient;
+        this.retrySpec = retrySpec;
+    }
+
+    @Override
+    public Mono<PullRequestResponse> fetchPullRequestDetails(String owner, String repo, int pullRequestId) {
+        return webClient
+                .get()
+                .uri("/repos/{owner}/{repo}/pulls/{pullRequestId}", owner, repo, pullRequestId)
+                .retrieve()
+                .onStatus(
+                        status -> status.is4xxClientError() || status.is5xxServerError(),
+                        response -> Mono.error(new RuntimeException("API Error")))
+                .bodyToMono(PullRequestResponse.class)
+                .retryWhen(retrySpec);
+    }
+
+    @Override
+    public Flux<IssuesCommentsResponse> fetchIssueComments(String owner, String repo, int issueNumber) {
+        return webClient
+                .get()
+                .uri("/repos/{owner}/{repo}/issues/{issueNumber}/comments", owner, repo, issueNumber)
+                .retrieve()
+                .bodyToFlux(IssuesCommentsResponse.class)
+                .retryWhen(retrySpec);
+    }
+
+    @Override
+    public Flux<PullCommentsResponse> fetchPullComments(String owner, String repo, int pullNumber) {
+        return webClient
+                .get()
+                .uri("/repos/{owner}/{repo}/pulls/{issueNumber}/comments", owner, repo, pullNumber)
+                .retrieve()
+                .bodyToFlux(PullCommentsResponse.class)
+                .retryWhen(retrySpec);
+    }
+}
---
+++ scrapper/src/main/java/backend/academy/scrapper/client/stackoverflow/StackOverflowClientImpl.java
@@ -0,0 +1,66 @@
+package backend.academy.scrapper.client.stackoverflow;
+
+import backend.academy.scrapper.dto.AnswerResponse;
+import backend.academy.scrapper.dto.AnswersApiResponse;
+import backend.academy.scrapper.dto.QuestionResponse;
+import backend.academy.scrapper.dto.QuestionsApiResponse;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+@Service
+public class StackOverflowClientImpl implements StackOverflowClient {
+
+    private static final String SITE = "site";
+    private static final String STACKOVERFLOW = "stackoverflow";
+    private static final String API_ERROR = "API Error";
+
+    private final WebClient webClient;
+    private final Retry retrySpec;
+
+    public StackOverflowClientImpl(@Qualifier("stackOverflowWebClient") WebClient webClient, Retry retrySpec) {
+        this.webClient = webClient;
+        this.retrySpec = retrySpec;
+    }
+
+    @Override
+    public Mono<List<QuestionResponse>> fetchQuestionsInfo(List<String> questionIds) {
+        String joinedQuestionIds = String.join(";", questionIds);
+
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/questions/{ids}")
+                        .queryParam(SITE, STACKOVERFLOW)
+                        .build(joinedQuestionIds))
+                .retrieve()
+                .onStatus(
+                        status -> status.is4xxClientError() || status.is5xxServerError(),
+                        response -> Mono.error(new RuntimeException(API_ERROR)))
+                .bodyToMono(QuestionsApiResponse.class)
+                .map(QuestionsApiResponse::getItems)
+                .retryWhen(retrySpec);
+    }
+
+    @Override
+    public Mono<List<AnswerResponse>> fetchAnswersInfo(List<String> questionIds) {
+        String joinedQuestionIds = String.join(";", questionIds);
+
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/questions/{id}/answers")
+                        .queryParam(SITE, STACKOVERFLOW)
+                        .build(joinedQuestionIds))
+                .retrieve()
+                .onStatus(
+                        status -> status.is4xxClientError() || status.is5xxServerError(),
+                        response -> Mono.error(new RuntimeException(API_ERROR)))
+                .bodyToMono(AnswersApiResponse.class)
+                .map(AnswersApiResponse::getItems)
+                .retryWhen(retrySpec);
+    }
+}
