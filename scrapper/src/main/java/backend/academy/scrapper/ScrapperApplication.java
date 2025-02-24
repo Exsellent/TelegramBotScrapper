a1c56beafdb0f1912eef5f25bc23fb97c5402ea7
Excellent <7074172@gmail.com>
2025-02-24 19:11:03 +0300

    Creating a startup class for the scrapper application

--- scrapper/src/main/java/backend/academy/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/backend/academy/scrapper/ScrapperApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
-@EnableConfigurationProperties({ScrapperConfig.class})
+@SpringBootApplication(
+        exclude = {
+            org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+            org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+            org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class
+        })
+@EnableConfigurationProperties(ScrapperConfig.class)
 public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);
