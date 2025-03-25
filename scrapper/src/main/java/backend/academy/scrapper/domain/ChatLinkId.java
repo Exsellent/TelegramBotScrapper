bdbfe93254286ec6a57be48b2578216b45ace34a
Excellent <7074172@gmail.com>
2025-03-26 00:10:46 +0300

    Add domain entities for Scrapper application

--- scrapper/src/main/java/backend/academy/scrapper/domain/ChatLinkId.java
+++ scrapper/src/main/java/backend/academy/scrapper/domain/ChatLinkId.java
@@ -1,18 +1,47 @@
 package backend.academy.scrapper.domain;
 
 import java.io.Serializable;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import java.util.Objects;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
 public class ChatLinkId implements Serializable {
+
     private Long chatId;
     private Long linkId;
+
+    public ChatLinkId() {}
+
+    public ChatLinkId(Long chatId, Long linkId) {
+        this.chatId = chatId;
+        this.linkId = linkId;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public Long getLinkId() {
+        return linkId;
+    }
+
+    public void setLinkId(Long linkId) {
+        this.linkId = linkId;
+    }
+
+    // Переопределение equals и hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChatLinkId that = (ChatLinkId) o;
+        return Objects.equals(chatId, that.chatId) && Objects.equals(linkId, that.linkId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chatId, linkId);
+    }
 }
---
+++ scrapper/src/main/java/backend/academy/scrapper/domain/Tag.java
@@ -0,0 +1,41 @@
+package backend.academy.scrapper.domain;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Link> links = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(Set<Link> links) {
+        this.links = links;
+    }
+}
